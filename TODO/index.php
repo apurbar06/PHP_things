5ba0a38a9d2264ba59858995e6a42f2d5fbe6550
apurbar06 <apurbar06@gmail.com>
2022-12-08 00:09:41 +0530

    added css and fonts for TODO

--- TODO/index.php
+++ TODO/index.php
@@ -5,11 +5,6 @@
 		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
 	</head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="https://sourcecodester.com">Sourcecodester</a>
-		</div>
-	</nav>
 	<div class="col-md-3"></div>
 	<div class="col-md-6 well">
 		<h3 class="text-primary">PHP - Simple To Do List App</h3>
